c7b2f83b0d8d632ffa7f483355cdbb853932889e
Urusofff <mks.urusov@gmail.com>
2023-08-31 19:08:02 +0300

    Lesson3VariablesAndPrimitives

---
+++ src/Lesson2NewHelloWorldProject.java
@@ -0,0 +1,16 @@
+
+/**
+ * @author Maxim Urusov
+ *
+ * @see <a href="https://vk.com/video-221759623_456239021">
+ *     Lesson 2 - new "HelloWorld!" project</a>
+ */
+public class Lesson2NewHelloWorldProject {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+    }
+
+}
---
+++ src/Lesson3VariablesAndPrimitives.java
@@ -0,0 +1,27 @@
+
+/**
+ * @author Maxim Urusov
+ *
+ * @see <a href="https://vk.com/video-221759623_456239023">
+ *     Lesson 3 - variables and primitives</a>
+ */
+public class Lesson3VariablesAndPrimitives {
+
+    public static void main(String[] args) {
+        byte newByte = 127;
+        short newShort = 32000;
+        int newInt = 2000000000;
+
+        long newLong = 20000000000L;
+        System.out.println(newLong);
+
+        float newFloat = 0.123f;
+        double newDouble = 0.123d;
+
+        char newChar = 'i';
+        boolean newBoolean = true;
+
+
+    }
+
+}
