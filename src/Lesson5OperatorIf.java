58a9bc58e4be40c7c13a73b01fecde724f3ff959
Urusofff <mks.urusov@gmail.com>
2023-09-13 15:07:20 +0300

    for home work

--- src/Lesson5OperatorIf.java
+++ src/Lesson5OperatorIf.java
@@ -9,12 +9,12 @@ public class Lesson5OperatorIf {
 
     public static void main(String[] args) {
 
-        boolean isNeededJava = false; // 1 - измените на false, чтобы попасть в else if
+        boolean isNeededJava = true; // 1 - измените на false, чтобы попасть в else if
 
         // полная форма
         if (isNeededJava) {
             System.out.println("Мы изучаем курс Notes about Java!");
-        } else if (2 + 2 != 4) { // 2 - измените на 2 + 2 != 4, чтобы попасть в else
+        } else if (2 + 2 == 4) { // 2 - измените на 2 + 2 != 4, чтобы попасть в else
             System.out.println("Здорово! Мы изучаем курс Notes about Java!");
         }
         // else if ...
