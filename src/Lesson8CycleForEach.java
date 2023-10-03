f4e3f353887ccfebff615090e8f76cf62d4ca3db
Urusofff <mks.urusov@gmail.com>
2023-10-03 17:28:46 +0300

    lesson 8

---
+++ src/Lesson8CycleForEach.java
@@ -0,0 +1,22 @@
+
+/**
+ * @author Maxim Urusov
+ * @see <a href="https://vk.com/video-221759623_456239033">
+ * 8 Урок - цикл for each</a>
+ */
+public class Lesson8CycleForEach {
+
+    public static void main(String[] args) {
+        forEach();
+    }
+
+    public static void forEach() {
+        System.out.println("Говорит цикл For Each в Java:");
+        byte[] numbers = {11, 22, 33}; //измените значение чтобы проверить работу цикла
+        byte counter = 1;
+        for (byte i : numbers) {
+            System.out.println(counter + "элемент массива = " + i);
+            counter++;
+        }
+    }
+}
