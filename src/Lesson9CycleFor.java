bd014a9cd81471886bd8e5cf914265b2399801f4
Urusofff <mks.urusov@gmail.com>
2023-10-10 16:55:49 +0300

    lesson 9

---
+++ src/Lesson9CycleFor.java
@@ -0,0 +1,20 @@
+
+/**
+ * @author Maxim Urusov
+ * @see <a href="https://vk.com/video-221759623_456239034">
+ * 9 Урок - цикл for</a>
+ */
+public class Lesson9CycleFor {
+
+    public static void main(String[] args) {
+        classicFor();
+    }
+
+    public static void classicFor() {
+        System.out.println("Говорит классический цикл For в Java:");
+        byte kilometres = 3; //измените значение чтобы проверить работу цикла
+        for (byte i = 1; i <= kilometres; i++) {
+            System.out.println("Проехал " + i + " км.");
+        }
+    }
+}
