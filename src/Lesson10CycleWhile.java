afabc23a576af835c90f24bc3fc0859f4f42e89f
Urusofff <mks.urusov@gmail.com>
2023-10-17 16:41:07 +0300

    lesson 10

---
+++ src/Lesson10CycleWhile.java
@@ -0,0 +1,25 @@
+
+/**
+ * @author Maxim Urusov
+ * @see <a href="https://vk.com/video-221759623_456239035">
+ * 10 Урок - цикл while</a>
+ */
+public class Lesson10CycleWhile {
+
+    public static void main(String[] args) {
+        whileLoop();
+    }
+
+    public static void whileLoop() {
+        byte kilometres = 3;
+//        while (true) { // true для бесконечного цикла
+        while (kilometres >= 0) { // закомментируйте при включении while (true)
+            System.out.println("Осталось до финиша: " + kilometres + "км.");
+//            if (kilometres < 2) { // расскомтрируйте чтобы остановить цикл
+//                System.out.println("Недоехал...");
+//                break;
+//            }
+            kilometres--;
+        }
+    }
+}
