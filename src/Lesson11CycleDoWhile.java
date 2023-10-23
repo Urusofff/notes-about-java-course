4bbe16c628d4d211483e2930e12e59b4d537385f
Urusofff <mks.urusov@gmail.com>
2023-10-23 17:33:48 +0300

    lesson 11

---
+++ src/Lesson11CycleDoWhile.java
@@ -0,0 +1,20 @@
+
+/**
+ * @author Maxim Urusov
+ * @see <a href="https://vk.com/video-221759623_456239036">
+ * 11 Урок - цикл do while</a>
+ */
+public class Lesson11CycleDoWhile {
+
+    public static void main(String[] args) {
+        doWhileLoop();
+    }
+
+    public static void doWhileLoop() {
+        byte kilometres = 3; // попробуйте сменить на 0 и запустить
+        do {
+            System.out.println("Осталось до финиша: " + kilometres + "км.");
+            kilometres--;
+        } while (kilometres >= 0);
+    }
+}
