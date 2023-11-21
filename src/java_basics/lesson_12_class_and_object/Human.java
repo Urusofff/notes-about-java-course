3c46b71d5bab0d2b6b35f3589e73f3f017f24607
Urusofff <mks.urusov@gmail.com>
2023-11-21 12:05:40 +0300

    lesson 12 - class and object

---
+++ src/java_basics/lesson_12_class_and_object/Human.java
@@ -0,0 +1,26 @@
+package java_basics.lesson_12_class_and_object;
+
+/**
+ * @author Maxim Urusov
+ * @see <a href="https://vk.com/video-221759623_456239038">
+ * 12 Урок - класс и объект</a>
+ */
+public class Human {
+
+    String name;
+    int age;
+
+    public static void main(String[] args) {
+
+        Human boy = new Human();
+        boy.age = 6; // измените возраст мальчика и запустите
+        boy.name = "Серафим";
+        System.out.println("Мальчика зовут " + boy.name + " и ему " + boy.age + " лет.");
+
+        Human girl = new Human();
+        girl.age = 4;
+        girl.name = "София"; // измените имя девочки и запустите
+        System.out.println("Девочку зовут " + girl.name + " и ей " + girl.age + " года.");
+
+    }
+}
