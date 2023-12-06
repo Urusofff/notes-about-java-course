b5cbba1ffa6d8d8fc5ae40f18af3edf04acedec5
Urusofff <mks.urusov@gmail.com>
2023-12-06 12:50:31 +0300

    Lesson13ReferenceTypeConcept

---
+++ src/java_basics/Lesson13ReferenceTypeConcept.java
@@ -0,0 +1,32 @@
+package java_basics;
+
+import java_basics.lesson_12_class_and_object.Human;
+
+/**
+ * @author Maxim Urusov
+ * @see <a href="https://vk.com/video-221759623_456239039">
+ * 13 Урок - понятие о ссылочном типе</a>
+ */
+public class Lesson13ReferenceTypeConcept {
+
+    public static void main(String[] args) {
+
+//        int boy = new Human(); // попробуйте присвоить ссылочный тип Human - примитиву
+
+        // аналогия - почта у человека ivanov@company.ru
+        Human man = new Human(); // инициализируйте man = null и запустите!
+        System.out.println("man - " + man);
+
+        Human woman = new Human();
+        System.out.println("woman - " + woman);
+
+        Human boy = new Human();
+        System.out.println("boy - " + boy);
+
+        Human girl = new Human();
+        System.out.println("girl - " + girl);
+
+        byte seven = 7; // попробуйте инициализировать seven = null
+        System.out.println("seven - " + seven);
+    }
+}
